bb4f135a93abbe548a32b926c3e08607ea75a741
Andreas von Studnitz <avs@avs-webentwicklung.de>
2016-06-23 09:34:06 +0200

    Don't assign customer groups automatically (fixes #2)

--- src/app/code/community/IntegerNet/EuropeanTax/sql/integernet_europeantax_setup/upgrade-0.3.0-0.4.0.php
+++ src/app/code/community/IntegerNet/EuropeanTax/sql/integernet_europeantax_setup/upgrade-0.3.0-0.4.0.php
@@ -5,7 +5,7 @@ $installer = Mage::getResourceModel('sales/setup', 'sales_setup');
 $installer->startSetup();
 
 $installer->setConfigData('customer/address/taxvat_show', '');
-$installer->setConfigData('customer/create_account/auto_group_assign', 1);
+$installer->setConfigData('customer/create_account/auto_group_assign', 0);
 $installer->setConfigData('customer/create_account/tax_calculation_address_type', 'shipping');
 $installer->setConfigData('customer/create_account/vat_frontend_visibility', 1);
 
